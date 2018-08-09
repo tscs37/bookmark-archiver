233b2063a3d4f4f86c922478e8dac862a8bbc5a6
Tim Schuster <mail@timschuster.info>
2018-08-09 06:08:50 +0200

    Disable screenshots for now

--- run_archive.sh
+++ run_archive.sh
@@ -4,9 +4,9 @@ set -euxo pipefail
 export TIMEOUT=120
 export FETCH_WGET=True
 export FETCH_PDF=True
-export FETCH_SCREENSHOT=True
+export FETCH_SCREENSHOT=False
 export FETCH_FAVICON=True
 export FETCH_ARCHIVE_DOT_ORG=True
 export WGET_USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"
 
-./archive https://bm.timschuster.info/?do=atom&nb=all
+./archive 'https://bm.timschuster.info/?do=atom&nb=all'

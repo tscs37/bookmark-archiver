a3e7f61c7f4fc65a6f2f012076c072217f513be1
Tim Schuster <mail@timschuster.info>
2018-12-06 08:26:28 +0100

    Merged

--- run_archive.sh
+++ run_archive.sh
@@ -8,5 +8,6 @@ export FETCH_SCREENSHOT=False
 export FETCH_FAVICON=True
 export FETCH_ARCHIVE_DOT_ORG=True
 export WGET_USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"
+export ONLY_NEW=True
 
 ./archiver/archive.py 'https://bm.timschuster.info/?do=atom&nb=all'
